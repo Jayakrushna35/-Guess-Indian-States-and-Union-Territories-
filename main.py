b2fb443dc4a790b7ada87cccc1350a89bf2d4068
Jayakrushna35 <jayakrushnapattnaik6@gmail.com>
2024-06-27 19:46:05 +0530

    Project is ready

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle,Screen
+import pandas as pd
 
 
 screen = Screen()
@@ -8,10 +9,29 @@ screen.addshape(image_path)
 jkp = Turtle()
 jkp.shape(image_path)
 
+data = pd.read_csv("ind_state.csv")
+all_states = data.state.to_list()
+guessed_states = []
 
-def get_mouse_click(x,y):
-     print(x,y)
-screen.onscreenclick(get_mouse_click)
-screen.mainloop()
+while len(guessed_states) < 29 :
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/29 State Correct", prompt="What's another state's name? ").title()
+     
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        j = Turtle()
+        j.hideturtle()
+        j.penup()
+        state_data = data[data.state == answer_state]
+        j.goto(int(state_data.X), int(state_data.Y))
+        j.write(answer_state)
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        df = pd.DataFrame(missing_states)
+        df.to_csv("state to learn.csv")       
+        break
 
-screen.exitonclick()
+
+
